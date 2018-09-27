bdc48ce00fc20aaf47030725979dbb5cdab22b5b
Nada <nada.galal6123@gamail.com>
2018-09-27 08:12:08 +0200

    Finally

--- hospital2/app/src/main/java/com/example/nadag/hospital2/Deppartments.java
+++ hospital2/app/src/main/java/com/example/nadag/hospital2/Deppartments.java
@@ -12,16 +12,25 @@ public class Deppartments extends AppCompatActivity {
     Button Pediatric;
     Button Dental;
     Button Physical_Therapy;
-
+    Button btBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deppartments);
        // getSupportActionBar().hide();
+        btBack=(Button)findViewById(R.id.back);
         Obtical=(Button) findViewById(R.id.bb3);
         Pediatric= (Button) findViewById(R.id.bb4);
         Dental= (Button) findViewById(R.id.bb2);
         Physical_Therapy=(Button)findViewById(R.id.bb1);
+       btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(Deppartments.this, MainActivity.class);
+                startActivity(i2);
+                finish();
+            }
+        });
         Physical_Therapy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
